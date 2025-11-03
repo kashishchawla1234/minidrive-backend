303aa4a57fc21a57b8fa9cd4e3ac2d49d742eb07
kashishchawla1234 <kashishchawla989@gmail.com>
2025-11-04 04:23:12 +0530

    Fix: MongoDB connection, login auth, admin file fetch

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,14 +1,3 @@
-// import File from "../models/File.js";
-// import User from "../models/User.js";
-
-// export const getAllFiles = async (req, res) => {
-//   const admin = await User.findById(req.user._id);
-//   if (!admin?.isAdmin) return res.status(403).json({ message: "Forbidden" });
-
-//   const files = await File.find().populate("owner", "email");
-//   res.json(files.map(f => ({ ...f._doc, ownerEmail: f.owner.email })));
-// };
-
 import File from "../models/File.js";
 
 export const getAllFiles = async (req, res) => {
